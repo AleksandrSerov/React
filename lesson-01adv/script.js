86a34b457ae6769f2af23febfbe5eae74379b104
Aleksandr Serov <sashaserov2010@gmail.com>
2019-01-11 09:47:02 +0300

    Optimize promise

--- lesson-01adv/script.js
+++ lesson-01adv/script.js
@@ -1,43 +1,23 @@
-let images = ['img/img_1.jpg','img/img_2.jpg', 'img/img_3.jpg'];
-let imgs = document.querySelectorAll('img');
+let images = ['http://alserov.ru/React/lesson-01adv/img/img_1.jpg',
+'http://alserov.ru/React/lesson-01adv/img/img_2.jpg', 
+'http://alserov.ru/React/lesson-01adv/img/img_3.jpg'];
 
-for (let i = 0; i < imgs.length; i++) {
-	imgs[i].style = 'display: none';
-	imgs[i].setAttribute('src', images[i]);
+function loadImage(path) {
+	return new Promise((resolve, reject) => {
+		let image = new Image();
+		image.src = path;
+		image.onload = () => resolve(image);
+		image.onerror = () => reject(path);
+	});
 }
-// Без промисов
-// imgs[0].addEventListener('load', () => {
-// 	imgs[0].style = 'display: inline-block';
-// })
-
-// imgs[1].addEventListener('load', () => {
-// 	imgs[1].style = 'display: inline-block';
-// })
-// imgs[2].addEventListener('load', () => {
-// 	imgs[2].style = 'display: inline-block';
-// })
-
-// С промисами
-let limg0 = new Promise ((resolve, reject) => {
-	imgs[0].addEventListener('load', resolve());
-	imgs[0].addEventListener('error', reject(images[0]));
-})
-
-let limg1 = new Promise ((resolve, reject) => {
-	imgs[1].addEventListener('load', resolve());
-	imgs[1].addEventListener('error', reject(images[1]));
-})
-
-let limg2 = new Promise ((resolve, reject) => {
-	imgs[2].addEventListener('load', resolve());
-	imgs[2].addEventListener('error', reject(images[2]));
-})
-Promise.all([limg0, limg1, limg2]).then(values => {
-	for (item of imgs) {
-		item.style = 'display: inline-block';
+let promises = [];
+for (let item of images) {
+	promises[promises.length] = loadImage(item);
+}
+Promise.all(promises).then(value => {
+	for (let item of value) {
+		document.body.appendChild(item);
 	}
-}).catch(
-	reason => {
-		console.error(reason);
-	}
-)
+}).catch( reason => {
+	console.error(reason);
+})
